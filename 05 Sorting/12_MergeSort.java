5b17511501edaa7bd63431882c0305ef327b6246
ShrinathP <37506420+ShrinathP@users.noreply.github.com>
2026-02-17 19:47:13 +0530

    Update 12_MergeSort.java

--- 05 Sorting/12_MergeSort.java
+++ 05 Sorting/12_MergeSort.java
@@ -1,8 +1,4 @@
-package com.dsa.Sorting;
-
-import java.util.Arrays;
-
-public class MergeSort {
+//Striver Explanation
 
 	
 	//Merge Sort is divide and conquer algorithm
@@ -21,94 +17,71 @@ public class MergeSort {
 	             Call merge(arr, l, m, r)
 	             
 	*/
-	
-		//**1 WRITING THE SORT FUNCTION
-	void sort(int arr[], int l, int r) {
 
-		if(l< r) {
-			//Finding the middle point
-			int m = (l+r)/2;
-			
-			//left sort
-			sort(arr, l, m);
-			//right sort
-			sort(arr, m+1, r);
-			
-			//Merge the sorted halves
-			merge(arr, l , m , r);
-		}
-	
-	}
-	
-	
-	//***2 WRITING THE MERGE FUNCTION
-	//merges 2 subarrays of arr[]
-	//First subarray is arr[l...m]
-	//second subarray is arr[m+1...r]
-	private void merge(int[] arr, int l, int m, int r) {
-	
-		//1 Step
-		//Find the sizes of the 2 subarrays to be merged
-		int n1 = m-l+1;
-		int n2 = r-m;
-		
-		//2 Step
-		/*Create temp arrays*/ 
-		int[] L = new int[n1];
-		int[] R = new int[n2];
-		
-		//3 Step
-		/*Copy data into the temp arrays*/
-		for(int i=0; i<n1; i++) {
-			L[i] = arr[l+i];
-		}
-		for(int j=0; j<n2; j++) {
-			R[j] = arr[m+1+j];
-		}
-		
-		
-		//4 Step
-		/* Merge this temp arrays*/
-		// Initial indexes of the first 2 subarrays
-		int i=0, j = 0;
-		
-		// Initial index of the merged subarray
-		int k=l; //Start filling arr from k = l
-		while(i < n1 && j <n2) {
-			if(L[i] <= R[j]) {
-				arr[k] = L[i];
-				i++;
-			}
-			else {
-				arr[k] = R[j];
-				j++;
-			}
-			k++;
-		}
-		
-		/* Copy remaining elements of L[] if any */
-        //if size of n1 is greater than n2 some elements will remain uncopied
-		while(i < n1) {
-			arr[k]= L[i];
-			i++;
-			k++;
-		}
-  
-        /* Copy remaining elements of R[] if any */
-		//if size of n2 is greater than n1 some elements will remain uncopied
-        while (j < n2) { 
-            arr[k]= R[j]; 
-            j++;
+public class MergeSort {
+
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left >= right) return;   // Base case: 1 element
+
+        int mid = left + (right - left) / 2;
+
+        // 1️⃣ Sort left half
+        mergeSort(arr, left, mid);
+
+        // 2️⃣ Sort right half
+        mergeSort(arr, mid + 1, right);
+
+        // 3️⃣ Merge sorted halves
+        merge(arr, left, mid, right);
+    }
+
+    private static void merge(int[] arr, int left, int mid, int right) {
+
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] leftArr = new int[n1];
+        int[] rightArr = new int[n2];
+
+        // Copy data into temp arrays
+        for (int i = 0; i < n1; i++)
+            leftArr[i] = arr[left + i];
+
+        for (int j = 0; j < n2; j++)
+            rightArr[j] = arr[mid + 1 + j];
+
+        int i = 0, j = 0, k = left;
+
+        // Merge temp arrays back into original
+        while (i < n1 && j < n2) {
+            if (leftArr[i] <= rightArr[j]) {
+                arr[k] = leftArr[i];
+                i++;
+            } else {
+                arr[k] = rightArr[j];
+                j++;
+            }
             k++;
         }
-        
-        System.out.println("Array State after merge" + Arrays.toString(arr));
-	}
-	
-	static void printArray(int[] arr) {
-		for(int i=0; i<arr.length ; i++) {
-			System.out.println(arr[i]);
-		}
-	}
-	
+
+        // Copy remaining elements
+        while (i < n1) {
+            arr[k++] = leftArr[i++];
+        }
+
+        while (j < n2) {
+            arr[k++] = rightArr[j++];
+        }
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 9, 1, 3, 6};
+
+        mergeSort(arr, 0, arr.length - 1);
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
 }
